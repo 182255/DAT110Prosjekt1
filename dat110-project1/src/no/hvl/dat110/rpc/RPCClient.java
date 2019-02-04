7976ebf3ebd5d143794dfed87de75bd5efd548bb
Enah <Enah@192.168.10.124>
2019-02-04 13:11:01 +0100

    done?

--- dat110-project1/src/no/hvl/dat110/rpc/RPCClient.java
+++ dat110-project1/src/no/hvl/dat110/rpc/RPCClient.java
@@ -6,54 +6,58 @@ public class RPCClient {
 
 	private MessagingClient msgclient;
 	private Connection connection;
-	
+
 	public RPCClient(String server, int port) {
-	
-		msgclient = new MessagingClient(server,port);
+
+		msgclient = new MessagingClient(server, port);
 	}
-	
+
 	public void register(RPCStub remote) {
 		remote.register(this);
 	}
-	
+
 	public void connect() {
-		
+
 		// TODO: connect using the underlying messaging layer connection
 		connection = msgclient.connect();
-		
-//	    throw new RuntimeException("not yet implemented");
-			
+
+		// throw new RuntimeException("not yet implemented");
+
 	}
-	
+
 	public void disconnect() {
-		
+
 		// TODO: disconnect/close the underlying messaging connection
 		msgclient.connect().close();
-		
-//		throw new RuntimeException("not yet implemented");
-		
+
+		// throw new RuntimeException("not yet implemented");
+
 	}
-	
+
 	public byte[] call(byte[] rpcrequest) {
-		
+
 		byte[] rpcreply;
-		
-		/* TODO: 
-		
-		Make a remote call on the RPC server by sending a request message
-		and receive a reply message
-		
-		rpcrequest is the marshalled rpcrequest from the client-stub
-		rpctreply is the rpcreply to be unmarshalled by the client-stub
-		
-		*/
-		
+
+		connection.send(new Message(rpcrequest));
+		rpcreply = connection.receive().getData();
+
+		/*
+		 * TODO:
+		 * 
+		 * Make a remote call on the RPC server by sending a request message and receive
+		 * a reply message
+		 * 
+		 * rpcrequest is the marshalled rpcrequest from the client-stub rpctreply is the
+		 * rpcreply to be unmarshalled by the client-stub
+		 * 
+		 */
+
 		if (true) {
-		  throw new RuntimeException("not yet implemented");
+			throw new RuntimeException("not yet implemented");
 		}
-		
+
 		return rpcreply;
-		
+
 	}
 
 }
